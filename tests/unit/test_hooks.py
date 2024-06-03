ddb78c1ba1e8db60fefe8dc659f510c2a37d26a5
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-06-03 16:39:15 -0400

    feat: Add `logrotate` to rotate slurm logs
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- tests/unit/test_hooks.py
+++ tests/unit/test_hooks.py
@@ -20,7 +20,59 @@ Current Python-based hooks include:
   - `configure`
 """
 
+import os
+import pathlib
+
 from slurmhelpers import hooks
+
+mock_logrotate_config = """
+$SNAP_COMMON/var/log/slurm/*.log {
+    weekly
+    rotate 4
+    size=5M
+    create 640 slurm root
+    missingok
+    nocopytruncate
+    nomail
+    notifempty
+    noolddir
+    sharedscripts
+    compress
+    delaycompress
+    compresscmd $SNAP/bin/bzip2
+    compressext .bz2
+    postrotate
+        $SNAP/usr/bin/pkill -x --signal SIGUSR2 slurmctld
+        $SNAP/usr/bin/pkill -x --signal SIGUSR2 slurmd
+        $SNAP/usr/bin/pkill -x --signal SIGUSR2 slurmdbd
+        exit 0
+    endscript
+}
+""".strip()
+target_logrotate_config = """
+/var/snap/slurm/common/var/log/slurm/*.log {
+    weekly
+    rotate 4
+    size=5M
+    create 640 slurm root
+    missingok
+    nocopytruncate
+    nomail
+    notifempty
+    noolddir
+    sharedscripts
+    compress
+    delaycompress
+    compresscmd /snap/slurm/x1/bin/bzip2
+    compressext .bz2
+    postrotate
+        /snap/slurm/x1/usr/bin/pkill -x --signal SIGUSR2 slurmctld
+        /snap/slurm/x1/usr/bin/pkill -x --signal SIGUSR2 slurmd
+        /snap/slurm/x1/usr/bin/pkill -x --signal SIGUSR2 slurmdbd
+        exit 0
+    endscript
+}
+""".strip()
 
 
 class TestHooks:
@@ -31,9 +83,22 @@ class TestHooks:
         mocker.patch("pathlib.Path.chmod")
         mocker.patch("pathlib.Path.mkdir")
         mocker.patch("pathlib.Path.touch")
+        mocker.patch("pathlib.Path.read_text")
+        mocker.patch("pathlib.Path.write_text")
         mocker.patch("pathlib.Path.write_bytes")
         mocker.patch("dotenv.set_key")
         hooks.install(snap)
+
+    def test_setup_logrotate(self, mocker, snap) -> None:
+        """Test `_setup_logrotate` helper method."""
+        mocker.patch("pathlib.Path.read_text", return_value=mock_logrotate_config)
+        mocker.patch("pathlib.Path.write_text")
+        os.environ["SNAP"] = "/snap/slurm/x1"
+        os.environ["SNAP_COMMON"] = "/var/snap/slurm/common"
+
+        # Assert that logrotate template was rendered correctly.
+        hooks._setup_logrotate(snap)
+        pathlib.Path.write_text.assert_called_once_with(target_logrotate_config)
 
     def test_configure_hook(self, mocker, snap) -> None:
         """Test `configure` hook."""
